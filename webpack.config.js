8115a5edb83498254324d8d5ac7d5fde27d0b705
Aleksandr Polozhenskii <polozhenskii@gmail.com>
2022-10-14 22:38:43 +0300

    Set up css preprocessors and postcss plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,79 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
     entry: {
-        app: './src/index.js'
+        app: './src/index.js',
     },
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname, './dist'),
-        publicPath: '/dist'
+        publicPath: '/dist',
     },
     module: {
-        rules: [{
-            test: /\.js$/,
-            loader: 'babel-loader',
-            exclude: '/node_modules/'
-        }]
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: '/node_modules/',
+            },
+            {
+                test: /\.scss$/i,
+                use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                ],
+            },
+        ],
     },
     devServer: {
         client: {
-            overlay: true
+            overlay: true,
         },
         static: {
-            directory: path.join(__dirname)
-        }
-    }
-}
+            directory: path.join(__dirname),
+        },
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
+    ],
+};
