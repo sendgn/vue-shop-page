d1cc693b808785b08f05ea8a0dda771f3705d1dd
Aleksandr Polozhenskii <polozhenskii@gmail.com>
2022-10-14 19:18:59 +0300

    Configure webpack and babel

---
+++ src/js/common.js
@@ -0,0 +1,1 @@
+const city = address?.city;
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+module.exports = {
+    entry: {
+        app: './src/index.js'
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/dist'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: '/node_modules/'
+        }]
+    },
+    devServer: {
+        client: {
+            overlay: true
+        },
+        static: {
+            directory: path.join(__dirname)
+        }
+    }
+}
